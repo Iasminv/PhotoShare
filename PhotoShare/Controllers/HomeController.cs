9612a304afadbc1a90ce22266b6f286a0f8b1e42
Iasminv <veroneziasmin@gmail.com>
2025-01-14 10:54:16 -0400

    Add project files.

---
+++ PhotoShare/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoShare.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Constructor
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
